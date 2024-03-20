221d318aab106d9d668a6aac37b5e68f086cbab0
Nestor Júnior <nestorjunior@MacBook-Air-de-Nestor.local>
2024-03-20 19:35:40 -0300

    create header app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComments } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
   return (
     <>
       <header className={styles.header}>
-        <h2>Header!</h2>
+        <FontAwesomeIcon icon={faComments} className='icon-logo' color="#00875f" size="2x" />
       </header>
     </>
   )
