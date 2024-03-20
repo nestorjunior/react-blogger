3693299d336539fa471ae143af7b3ce64f9e401f
Nestor Júnior <nestorjunior@MacBook-Air-de-Nestor.local>
2024-03-20 18:09:08 -0300

    create global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Header } from './components/Header'
+import { Posts } from './components/Posts'
+import './global.css'
 
 export function App() {
   return (
     <>
-      <h1>React Blogger</h1>
+      <Header />
+      <Posts author="Nestor Pereira" description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis vitae laboriosam repudiandae laudantium, hic provident tenetur vel! Laudantium porro distinctio, eos et omnis non fugiat quo maxime neque? Architecto, vel." />
+      <Posts author="Adriane" description="Perferendis vitae laboriosam repudiandae laudantium, hic provident tenetur vel! Laudantium porro distinctio, eos et omnis non fugiat quo maxime neque? Architecto, vel." />
+      <Posts author="Joelson" description="Distinctio, eos et omnis non fugiat quo maxime neque? Architecto, vel." />
     </>
   )
 }
+
 
 
---
+++ src/components/Posts.jsx
@@ -0,0 +1,9 @@
+export function Posts(props) {
+  return (
+    <>
+      <h3>{ props.author }</h3>
+      <p>{ props.description }</p>
+      <hr/>
+    </>
+  )
+}
